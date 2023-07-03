19f5fef96bf3ec0c48541a44efb43a60ea7c914c
ddusichka <78527291+ddusichka@users.noreply.github.com>
2023-07-03 10:25:40 -0400

    fixing build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="h-screen flex-col items-center justify-between p-24">
-      <p className="text-3xl text-black">Hi, I'm Dessy!</p>
+      <p className="text-3xl text-black">Hi!</p>
     </main>
   );
 }
