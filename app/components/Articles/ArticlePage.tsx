433b3947c57dc23014b7556ff69eb8e269998443
ddusichka <78527291+ddusichka@users.noreply.github.com>
2023-12-27 13:45:37 -0500

    draft article page

--- app/components/Articles/ArticlePage.tsx
+++ app/components/Articles/ArticlePage.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-const ProjectPage = (proj: {
+const ArticlePage = (proj: {
   title: string;
   subtitle: string;
   issue: string;
@@ -13,11 +13,24 @@ const ProjectPage = (proj: {
 }) => {
   return (
     <div className="">
-      <h1>{proj.issue}</h1>
-      {/* <h3>
-        {proj.title}: {proj.subtitle}
-      </h3> */}
-      <p>{proj.summary}</p>
+      <div className="flex flex-col tablet:flex-row">
+        <div className="tablet:w-4/5 tablet:mr-6">
+          <h2 className="text-3xl font-bold">{proj.title}</h2>
+          <h2 className="text-2xl font-semibold mb-2">{proj.subtitle}</h2>
+          <p className="mb-4 text-lg">{proj.summary}</p>
+          <p className="text-gray-600 font-semibold text-lg">
+            Written for NU Sci {proj.issue}, {proj.date}
+          </p>
+        </div>
+
+        <div className="tablet:w-1/5">
+          <img
+            src={proj.cardImage}
+            alt={proj.title}
+            className="w-full max-h-96 mb-4"
+          />
+        </div>
+      </div>
       <div className="flex flex-col items-center">
         <iframe
           src={`${proj.articlepdf}/#toolbar=0&view=fitW`}
@@ -28,4 +41,4 @@ const ProjectPage = (proj: {
   );
 };
 
-export default ProjectPage;
+export default ArticlePage;
