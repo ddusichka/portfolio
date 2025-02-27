926f3e56895afb3cb880f4daa7411ba1d89bb18a
ddusichka <78527291+ddusichka@users.noreply.github.com>
2025-02-26 23:45:44 -0500

    add content

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 import Icons from "./Icons";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div className="ml-4">
-      <hr className="h-px my-2 bg-gray-400 border-0" />
-      <div className="flex justify-between tablet:justify-normal text-lg pb-2">
+    <div className={styles.container}>
+      <hr className={styles.divider} />
+      <div className={styles.content}>
         <p>
-          © 2024 | Built by
+          © 2025 | Built by
           <a href="mailto:dusichka.d@northeastern.edu" target="_blank">
-            <span className="ml-1 font-bold text-[#81559b]">
-              Dessy Dusichka
-            </span>
+            <span className={styles.link}>Dessy Dusichka</span>
           </a>
         </p>
-        <div className="ml-2 tablet:ml-6 mr-4 mt-1 flex gap-2 laptop:gap-4 justify-center">
+        <div className={styles.iconsContainer}>
           <Icons color="black" size="1em" />
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Footer;
+const styles = {
+  container: "ml-4",
+  divider: "h-px my-2 bg-gray-400 border-0",
+  content: "flex justify-between tablet:justify-normal text-lg pb-2",
+  link: "ml-1 font-bold text-[#81559b]",
+  iconsContainer:
+    "ml-2 tablet:ml-6 mr-4 mt-1 flex gap-2 laptop:gap-4 justify-center",
+};
