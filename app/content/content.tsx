ffd1a0707d0b5987592a78035136a56749ee1ff8
ddusichka <78527291+ddusichka@users.noreply.github.com>
2025-04-12 16:16:09 -0400

    arenius!

--- app/content/content.tsx
+++ app/content/content.tsx
@@ -26,6 +26,55 @@ const content = {
   ],
   projects: [
     {
+      name: "Arenius",
+      path: "arenius",
+      emoji: "🌳",
+      dates: "January 2025 - April 2025",
+      deployed: false,
+      featured: true,
+      url: "",
+      shortDescription:
+        "A carbon-accounting platform to empower small-to-medium sized businesses to track, manage, and reduce their carbon footprint.",
+      longDescription:
+        "Arenius is a carbon accounting web application designed for small-to-medium sized businesses. It allows users to track their carbon footprint, manage their emissions, and take steps to reduce their impact on the environment. The platform provides a user-friendly interface for businesses to input their data and receive insights into their carbon emissions. Company bank transactions are imported from their 3rd-party accounting software and their carbon footprint is estimated based on assigned carbon emission factors.",
+      cardPhoto: "/areniusCover.png",
+      coverPhotos: ["/areniusDashboard.png"],
+      skills: ["Go", "TypeScript", "React"],
+      details: (
+        <>
+          <div className="tablet:flex justify-between">
+            <div className="flex flex-col gap-8 mr-8 w-3/5">
+              <p>
+                This app was developed for a real client as part of Generate at
+                Northeastern, a student-led product development studio. As one
+                of two tech leads, I managed a team of 5 engineers and created
+                tickets, reviewed code, and engineered features. We worked
+                alongside a team of designers, helping to bring their visuals to
+                life in a responsive and efficient web application.
+              </p>
+              <p>
+                The engineering team began by building the backend in Go and
+                integrating with external APIs. Specifically, we integrated with
+                Xero, an accounting API, and Climatiq, a carbon accounting API
+                for estimating carbon emissions based on financial transactions.
+                Once the design team created Figma mockups, we built the
+                front-end using React, Next.js, Tailwind, and the shadcn
+                component library.
+              </p>
+              <p>
+                This was my first time learning about the carbon accounting
+                space and making a data-focused app that revolves around
+                visualizations. It was very rewarding to work on this project
+                with my team and complete all of the features that we set out to
+                accomplish.
+              </p>
+            </div>
+            <img src={"/areniusTeam.jpg"} className=" w-2/5" />
+          </div>
+        </>
+      ),
+    },
+    {
       name: "Couplet",
       path: "couplet",
       emoji: "🎟️",
