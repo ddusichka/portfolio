cf37ba3e7e75fd341d35aa86e93fe178892dbc4e
ddusichka <78527291+ddusichka@users.noreply.github.com>
2025-06-22 21:49:52 -0400

    mobile friendly attempt

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import ClickableDesk from "./components/Home/ClickableDesk";
 export default function Home() {
   return (
     <div className="flex justify-center py-4 laptop:pt-12 bg-primary">
-      <div className="w-[80%] flex flex-col space-y-6 text-white">
+      <div className="tablet:w-[80%] flex flex-col space-y-6 text-white min-h-screen">
         <div className="flex flex-col tablet:flex-row justify-between items-center">
           <Image src={"/profile.png"} alt="Banner" width={100} height={100} />
           <h1 className="text-4xl tablet:text-6xl mb-4 ml-8 tablet:mb-6 w-full">
@@ -20,9 +20,9 @@ export default function Home() {
             <TypingEffect />
           </div>
         </div>
-        <div>
-          <ClickableDesk />
-        </div>
+        {/* <div> */}
+        <ClickableDesk />
+        {/* </div> */}
       </div>
     </div>
   );
